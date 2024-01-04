10ba62f60bf5aafdf5066b64162c4e89b2827022
Mark Frymire <mark.frymire@icloud.com>
2024-01-04 13:39:35 -0500

    Explicitly test for failures with assert demo

--- src/demo/v_3_10/numpy_demo/testing.py
+++ src/demo/v_3_10/numpy_demo/testing.py
@@ -1,19 +1,11 @@
 import unittest
 
-from numpy import array, floor, log10
+from numpy import array, floor, log10, all
 from numpy.testing import assert_allclose
+from pytest import raises, approx
 
 
-class TestCloseArrays(unittest.TestCase):
-
-    computed = array([0.0123012, 0.456, 0.789])
-    expected = array([0.0123000, 0.456, 0.789])
-
-    # Counterintuitively, assert_allclose() sets the tolerance to absolute *plus* rtol * abs(desired), rather than
-    # testing for absolute tolerance only in the case of small values, as in MATLAB. Generally, just use one or
-    # the other. Relative tolerance is always measured with 'desired' as the denominator value, not
-    # min(actual, desired). Therefore, rtol is a threshold on abs(actual - desired) / abs(desired). Unfortunately,
-    # it also doesn't report which element failed.
+class AssertSignificantDigits:
 
     @staticmethod
     def base10_exponent(number):
@@ -24,12 +16,59 @@ class TestCloseArrays(unittest.TestCase):
 
     @staticmethod
     def absolute_tolerance_for_n_digits(desired, significant_digits=3):
-        return 0.5 * 10**(TestCloseArrays.base10_exponent(desired) - (significant_digits - 1))
+        return 0.5 * 10**(AssertSignificantDigits.base10_exponent(desired) - (significant_digits - 1))
 
-    def test(self):
+    @staticmethod
+    def assert_significant_digits_match(computed, expected, significant_digits=3):
+        for a, b in zip(computed, expected):
+            atol = AssertSignificantDigits.absolute_tolerance_for_n_digits(b, significant_digits)
+            assert a == approx(b, abs=atol)
 
-        assert_allclose(self.computed, self.expected, rtol=1e-4, atol=0)
 
-        atol = self.absolute_tolerance_for_n_digits(self.expected[0], 4)
-        # atol = self.absolute_tolerance_for_n_digits(self.expected[0], 5)  # fails
-        assert_allclose(self.computed, self.expected, rtol=0, atol=atol)
+class TestAssertClose(unittest.TestCase):
+
+    expected = array([0.0123000, 0.456, 0.789])
+
+    # Counterintuitively, assert_allclose() sets the tolerance to absolute *plus* rtol * abs(desired), rather than
+    # testing for absolute tolerance only in the case of small values, as in MATLAB and pytest. Generally, just use one
+    # or the other. Relative tolerance is always measured with 'desired' as the denominator value, not
+    # min(actual, desired). Therefore, rtol is a threshold on abs(actual - desired) / abs(desired). Unfortunately,
+    # it also doesn't report which element failed.
+    def test_relative_tolerance(self):
+
+        computed = array([0.0123012, 0.456, 0.789])
+        assert_allclose(computed, self.expected, rtol=1e-4, atol=0)
+
+        with raises(AssertionError, match=r"Not equal to tolerance rtol="):
+            computed = array([0.0123013, 0.456, 0.789])
+            assert_allclose(computed, self.expected, rtol=1e-4, atol=0)  # fails
+
+    def test_absolute_tolerance(self):
+
+        computed = array([0.0123012, 0.456, 0.789])
+
+        atol = AssertSignificantDigits.absolute_tolerance_for_n_digits(self.expected[0], 4)
+        assert_allclose(computed, self.expected, rtol=0, atol=atol)
+
+        with raises(AssertionError, match=r"Not equal to tolerance rtol="):
+            atol = AssertSignificantDigits.absolute_tolerance_for_n_digits(self.expected[0], 5)
+            assert_allclose(computed, self.expected, rtol=0, atol=atol)  # fails
+
+    def test_pytest_approx(self):
+
+        # If you specify both ``abs`` and ``rel``, the numbers will be considered equal if either tolerance is met.
+        assert 1 + 1e-8 == approx(1)  # defaults are rel=1e-6 and abs=1e-12, must pass either
+        assert 1 + 1e-8 == approx(1, rel=1e-6, abs=1e-12)  # same
+        with raises(AssertionError):
+            assert 1 + 1e-8 == approx(1, abs=1e-12)
+
+        computed = array([0.0123012, 0.456, 0.789])
+
+        # Use numpy.all() to test all components. The output is nice, but you can't vary the tolerance by component.
+        with raises(AssertionError):
+            assert all(computed == approx(self.expected))
+
+        # Brute force method
+        AssertSignificantDigits.assert_significant_digits_match(computed, self.expected, 4)
+        with raises(AssertionError):
+            AssertSignificantDigits.assert_significant_digits_match(computed, self.expected, 5)
